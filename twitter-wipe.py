e73247d69230b2f2f4cb2df6d4eed7ccb70a0245
Alex Canepa <alex@canepa.me>
2021-02-05 17:51:26 -0500

    post new tweet; display user info

--- twitter-wipe.py
+++ twitter-wipe.py
@@ -8,8 +8,20 @@ with open(keypath, "r") as jsonfile:
 # Authenticate user session
 auth = tweepy.OAuthHandler(api_data["consumer"]["key"], api_data["consumer"]["secret"])
 auth.set_access_token(api_data["access"]["token"], api_data["access"]["secret"])
-api = tweepy.API(auth)
+api = tweepy.API(auth, timeout=5) # impatient
+print("User Authenticated...")
 
-public_tweets = api.user_timeline(user_id=api_data["username"])
-for tweet in public_tweets:
-    print(tweet.text)
+# Print my 20 most recent tweets
+myacct = api.me()
+for tweet in tweepy.Cursor(api.user_timeline, id=myacct.id).items(5):
+    print(tweet.id_str, tweet.text)
+
+# # Write a new tweet
+# api.update_status('tweepy + oauth!')
+
+# Show account information
+print(
+    "\nTwitter account @{} (display name `{}`) has tweeted {} times.".format(
+        myacct.screen_name, myacct.name, myacct.statuses_count
+    )
+)
